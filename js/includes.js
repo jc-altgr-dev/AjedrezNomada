1e57060792a6630e684e707ba2751eaa1f867cbc
jc-altgr-dev <kerno.da.neo@gmail.com>
2025-03-10 21:15:40 -0600

    Página sencilla con secciones autoreferenciadas

---
+++ js/includes.js
@@ -0,0 +1,9 @@
+// Script para cargar modularmente el header y footer
+function loadInclude(id, file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(data => document.getElementById(id).innerHTML = data);
+}
+loadInclude("header", "../includes/header.html");
+loadInclude("footer", "../includes/footer.html");
+
