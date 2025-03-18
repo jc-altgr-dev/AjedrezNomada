375931119d326a61486de73b24ebfb8b4fe2a74e
jc-altgr-dev <kerno.da.neo@gmail.com>
2025-03-18 13:00:19 -0600

    Enlaces entre subpáginas

--- js/includes.js
+++
@@ -1,9 +0,0 @@
-// Script para cargar modularmente el header y footer
-function loadInclude(id, file) {
-  fetch(file)
-    .then(res => res.text())
-    .then(data => document.getElementById(id).innerHTML = data);
-}
-loadInclude("header", "../AjedrezNomada/includes/header.html");
-loadInclude("footer", "../AjedrezNomada/includes/footer.html");
-
